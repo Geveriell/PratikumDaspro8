d9284bd8ef86bbda4567ad4036f682dbcf223bff
Geveriell <erzaardhyaputra@gmail.com>
2025-10-29 10:17:35 +0700

    modifikasi percobaan 4

--- NilaiKelompok07.java
+++ NilaiKelompok07.java
@@ -1,16 +1,19 @@
-//revisi percobaan 4 
+// Revisi Percobaan 4
 import java.util.Scanner;
 
 public class NilaiKelompok07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i = 1; 
-        while (i <= 6) { //6 kelompok
-            System.out.println("Kelompok " + i);
+        int i = 1;
+        double rataTertinggi = 0;
+        int kelompokTertinggi = 0;
+
+        while (i <= 6) { // 6 kelompok
+            System.out.println("\nKelompok " + i);
             double totalNilai = 0;
 
-            // inner loop 
+            // inner loop
             for (int j = 1; j <= 5; j++) {
                 System.out.print(" Nilai dari Kelompok Penilai " + j + " = ");
                 double nilai = sc.nextDouble();
@@ -19,8 +22,17 @@ public class NilaiKelompok07 {
 
             double rataNilai = totalNilai / 5;
             System.out.println("Rata-rata Nilai Kelompok " + i + " = " + rataNilai);
+
+            //rata-rata tertinggi
+            if (rataNilai > rataTertinggi) {
+                rataTertinggi = rataNilai;
+                kelompokTertinggi = i;
+            }
+
             i++;
         }
+        System.out.println("Kelompok dengan rata-rata tertinggi:");
+        System.out.println("Kelompok " + kelompokTertinggi + " dengan nilai rata-rata = " + rataTertinggi);
 
         sc.close();
     }
